8ea1f0f2da7b8d99cbc6dfe099a82457f07c3f43
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 11:39:00 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program will find the maximum interger from 3 intergers
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+  //create a scanner for user imput
+  Scanner input = new Scanner(System.in);
+
+  //ask for the intergers
+  System.out.println("Please enter three integers on separate lines");
+  int num1 = input.nextInt();
+  int num2 = input.nextInt();
+  int num3 = input.nextInt();
+
+  //determine the maximum interger
+  if(num1 >= num2 && num1 >= num3){
+    System.out.println("Maximum: " + num1);
+
+    } else if(num2 >= num1 && num2 >= num3){
+    System.out.println("Maximum: " + num2);
+    } else if(num3 >= num1 && num3 >= num2){
+    System.out.println("Maximum: " + num3);
+    }
+  
   }
 }
